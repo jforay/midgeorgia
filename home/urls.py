c0984c304dd53c7113f1a832f0c5d50a5ceabec9
Joe Foray <you@example.com>
2026-05-07 08:38:45 -0400

    new pages

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
+    path('', views.home, name='home'),
+    path('equipment/', views.equipment, name='equipment'),
+    path('first-responders/', views.first_responders, name='first_responders'),
+    path('dirty-work/', views.dirty_work, name='dirty_work'),
+    path('get-back/', views.get_back, name='get_back'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
     return render(request, 'home/home.html')
+
+def equipment(request):
+    return render(request, 'home/equipment.html')
+
+def first_responders(request):
+    return render(request, 'home/first_responders.html')
+
+def dirty_work(request):
+    return render(request, 'home/dirty_work.html')
+
+def get_back(request):
+    return render(request, 'home/get_back.html')
