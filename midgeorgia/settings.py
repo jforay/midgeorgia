f173494574f2a9603f1f8a7996571e2c89bc1440
Joe Foray <you@example.com>
2026-05-18 12:40:34 -0400

    adding email function

--- midgeorgia/settings.py
+++ midgeorgia/settings.py
@@ -129,12 +129,12 @@ STATIC_URL = 'static/'
 
 # Email (SendGrid)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY', '')
-DEFAULT_FROM_EMAIL = 'no-reply@midgeorgiaboatclub.com'
-SITE_ADMIN_EMAIL = 'joe4a83@gmail.com'
+EMAIL_HOST_USER = 'joe4a83@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'joe4a83@gmail.com'
+
 
 HONEYPOT_FIELD_NAME = 'phonenumber2'
